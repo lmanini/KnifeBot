58c38c60abd3070b4b1eec384db80696211a1ac4
lmanini <lmanini@protonmail.com>
2022-12-15 15:50:53 +0100

    Added knife and moo bal commands

--- backend/src/subcommands/subcommands.go
+++ backend/src/subcommands/subcommands.go
@@ -3,6 +3,7 @@ package subcommands
 import (
 	"KnifeBot/src/bindings"
 	"KnifeBot/src/constants"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -14,9 +15,34 @@ type Command uint8
 const (
 	SpyBalance Command = iota
 	SpyTopBalance
+	KnifeBalance
+	KnifeTopBalance
+	MooBalance
 )
 
 func SpyBalanceComm(target *common.Address) (uint64, error) {
+	return getSpyBal(target)
+}
+
+func KnifeBalanceComm(target *common.Address) (uint64, error) {
+	return getKnifeBal(target)
+}
+
+func MooBalanceComm(target *common.Address) (*big.Int, error) {
+	return getMooBal(target)
+}
+
+// sybil cluster getters
+
+// total spies
+
+// total knives
+
+// total moo ? moo is non transferable tho
+// --> calculate total moo but keep track 
+// of each addr in the cluster's balance 
+
+func getSpyBal(target *common.Address) (uint64, error) {
 	client, _ := ethclient.Dial(constants.RPC)
 	caller, err := bindings.NewSpyNFTCaller(common.HexToAddress(constants.SpyNFT),client)
 	bal, err := caller.BalanceOf(&bind.CallOpts{}, *target)
@@ -24,4 +50,24 @@ func SpyBalanceComm(target *common.Address) (uint64, error) {
 		return 0, err
 	}
 	return bal.Uint64(), nil
+}
+
+func getKnifeBal(target *common.Address) (uint64, error) {
+	client, _ := ethclient.Dial(constants.RPC)
+	caller, err := bindings.NewKnifeNFTCaller(common.HexToAddress(constants.KnifeNFT),client)
+	bal, err := caller.BalanceOf(&bind.CallOpts{}, *target)
+	if err != nil {
+		return 0, err
+	}
+	return bal.Uint64(), nil
+}
+
+func getMooBal(target *common.Address) (*big.Int, error) {
+	client, _ := ethclient.Dial(constants.RPC)
+	caller, err := bindings.NewSpyNFTCaller(common.HexToAddress(constants.SpyNFT),client)
+	bal, err := caller.GooBalance(&bind.CallOpts{}, *target)
+	if err != nil {
+		return big.NewInt(0), err
+	}
+	return bal, nil
 }
